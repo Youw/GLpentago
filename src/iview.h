9f85a2e49a77a2a388fc9b4095ff7a0fdceb8de7
Ihor Dutchak <ihor.youw@gmail.com>
2014-07-06 01:58:51 +0300

    iview bugfix and additional changes

--- src/iview.h
+++ src/iview.h
@@ -2,8 +2,8 @@
 #define IVIEW_H
 
 #include <string>
-#include <cstdint>
-#include <ctime>
+#include <cstdint>//uint32_t
+#include <ctime>//time_t
 #include <vector>
 
 using string = std::wstring;
@@ -12,59 +12,62 @@ using str_array =  std::vector<string>;
 class IView
 {
 public:
+
     enum class GAME_LAYOUT
     {
         MAIN_MENU,/*shows main menu*/
-		LOBBY,/*shows lobby*/
-		JOIN_GAME,/*shows list of available local hosts and edit for remote host address*/
-		SAVE_GAME,/*list of saves or new save*/
-		LOAD_GAME,/*show list of available saves*/
-		GAME
+	LOBBY,/*shows lobby*/
+	JOIN_GAME,/*shows list of available local hosts and edit for remote host address*/
+	SAVE_GAME,/*list of saves or new save*/
+	LOAD_GAME,/*show list of available saves*/
+	GAME
     };
 
-	enum class GAME_MODE {
-		MODE_PENTAGO, 		/* 6x6 board, 4 quadrants*/
-		MODE_PENTAGO_XL 	/* 9x9 board, 9 quadrants*/
-	};
+    enum class GAME_MODE {
+	MODE_PENTAGO, 		/* 6x6 board, 4 quadrants*/
+	MODE_PENTAGO_XL 	/* 9x9 board, 9 quadrants*/
+    };
 	
     enum class QUADRANT{	
-		III=2,	II=1,	IX=9,
-		IV=3,	I=4,	VIII=8,
-		V=5, 	VI=6,	VII=7	};/* do not ask, just look at the position in the enum... */
+	II=2,	I=1,	IX=9,
+	III=3,	IV=4,	VIII=8,
+	V=5, 	VI=6,	VII=7	
+    };/* do not ask, just look at the position in the enum... */
 
-	enum class DIRECTION{LEFT, RIGHT};      /*  for quadrant rotation   */
+    enum class DIRECTION{LEFT, RIGHT};      /*  for quadrant rotation   */
 
- 	enum class MESSAGE_TYPE{INFO, WARNING, ERROR, TIP};
+    enum class MESSAGE_TYPE{INFO, WARNING, ERROR, TIP};
 	
-	enum class MESSAGE_BUTTONS {
-		OK,
-		YES_NO,
-		YES_NO_CANCEL,
-		NO_BUTTONS /*no buttons*/
-	};
+    enum class MESSAGE_BUTTONS {
+	OK,
+	YES_NO,
+	YES_NO_CANCEL,
+	NO_BUTTONS /*no buttons*/
+    };
 	
-	enum class MESSAGE_ICON {
-		NONE,
-		INFO,
-		WORNING,
-		ERROR,
-		WAITING/*some circle or whatever*/
-	};
+    enum class MESSAGE_ICON {
+	NONE,
+	INFO,
+	WORNING,
+	ERROR,
+	WAITING/*some circle or whatever*/
+    };
 	
-	enum class MESSAGE_ANSWER {
-		OK,
-		YES,
-		NO,
-		CANCEL
-	};
+    enum class MESSAGE_ANSWER {
+	OK,
+	YES,
+	NO,
+	CANCEL
+    };
 	
-	enum class LOBBY_STATUS {
-		NORMAL, /*lobby just created*/
-		READY /*player ready to start and waiting for others*/
-	};
+    enum class LOBBY_STATUS {
+	NORMAL, /*lobby just created*/
+	READY /*player ready to start and waiting for others*/
+    };
 	
-public:
-
+public: //some kind of slots
+	virtual void Set_saves_list(const str_array& save_names,const str_array& saves_info)=0;
+	
 	virtual void Enable_chat()=0; //only while game or lobby
 	virtual void Disable_chat()=0; //only while game or lobby
 	
@@ -74,16 +77,16 @@ public:
 	//lobby_name = L"" --- do not change
 	//player_count < 0 --- do not change
 	virtual void Set_lobby_player_name(int player_num, const string& name)=0;
-	virtual void Set_lobby_player_color(int player_num, int rgb)=0;
+	virtual void Set_lobby_player_color(int player_num, int uint32_t)=0;
 	virtual void Set_lobby_player_color_charge_enable(int player_num, bool enabled)=0;
 	virtual void Set_lobby_player_avatar(int player_num, const char* image)=0;
 	//image can be as any format, like readed from the file (BMP,PNG,JPG)
 	
 	virtual void Set_hosts_list(const str_array& hosts)=0;
-    
+	
 	virtual void Clear_board()=0;/*remove all the stones*/
-	virtual void Put_stone(int row, int col, int grb)=0; /*put stone at [row, col] with color as rgb*/
-    virtual void Rotate_quadrant(QUADRANT quadrant, TURN turn)=0; //rotate selected quadrant to specified direction
+	virtual void Put_stone(int row, int col, uint32_t rgb)=0; /*put stone at [row, col] with color as rgb*/
+	virtual void Rotate_quadrant(QUADRANT quadrant, TURN turn)=0; //rotate selected quadrant to specified direction
 	
 	virtual void Disable_rotate_quadrant()=0;
 	virtual void Enable_rotate_quadrant()=0;
@@ -94,29 +97,33 @@ public:
 	/*show some text to player during the game*/
 	virtual void Hide_message()=0;/*ti hide current message*/
 	
+	virtual void Ask_user_text_input(const string& question, const string& button_ok_text);
+	
 	virtual void Clear_chat()=0;
 	virtual void Add_message_to_chat(string from, string text, time_t message_time)=0;
-public:
-    virtual void Request_show_lobby(int player_count)=0;//for local game only
+public://signals to presenter
+	virtual void Request_show_lobby(int player_count)=0;//for local game only
 	virtual void Request_lobby_ready()=0;//start game for local and "ready" for network game
-    virtual void Request_leave_lobby()=0;
+	virtual void Request_leave_lobby()=0;
 	
 	virtual void Request_get_saves_list()=0;
 	virtual void Request_save_game(const string& save_name)=0;
-    virtual void Request_load_game(const string& save_name)=0;
+	virtual void Request_load_game(const string& save_name)=0;
 	
 	virtual void Request_get_hosts_list()=0;
 	virtual void Request_join_game(const string& host_address)=0;
 	virtual void Request_host_game(const string& lobby_name, int player_count, const string& password = L"")=0;
     
 	virtual void Request_put_stone(int row, int col)=0;
-    virtual void Request_rotate_quadrant(QUADRANT quadrant, DIRECTION direction)=0;
+	virtual void Request_rotate_quadrant(QUADRANT quadrant, DIRECTION direction)=0;
 	
 	virtual void Request_send_to_chat(const string& message)=0;
 	
-	virtual void Request_answer_massage(MESSAGE_ANSWER answer)=0;
+	virtual void Request_massage_answer(MESSAGE_ANSWER answer)=0;
 	
-    virtual void Request_leave_game()=0;
+	virtual void Request_user_text_output(const string& text);
+	
+	virtual void Request_leave_game()=0;
 };
 
 #endif // IVIEW_H
